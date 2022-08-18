1ce35df2d9b9871cd1dee44834714b9c6aa19062
Dankisey <greeng3n@gmail.com>
2022-08-18 15:52:43 +0300

    Multy event subscribe bugfixing

--- Assets/Sources/Controller/MapController.cs
+++ Assets/Sources/Controller/MapController.cs
@@ -6,20 +6,30 @@ namespace Sapper.Controller
     public class MapController : MonoBehaviour
     {
         private Map _map;
+        private bool _controlsAreActive;
 
         public void Init(Map map)
         {
             _map = map;
+            _controlsAreActive = true;
         }
 
         public void DisableInput()
         {
+            if (_controlsAreActive == false)
+                return;
+
             _map.DisableInputHandlers();
+            _controlsAreActive = false;
         }
 
         public void EnableInput()
         {
+            if (_controlsAreActive)
+                return;
+            
             _map.EnableInputHandlers();
+            _controlsAreActive = true;
         }
     }
 }
